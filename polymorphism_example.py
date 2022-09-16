a98c35c68a9f3fdd7b13d7b0792390965dd857da
bhargav-borah <100614703+bhargav-borah@users.noreply.github.com>
2022-09-16 21:19:09 +0530

    Initial commit

---
+++ polymorphism_example.py
@@ -0,0 +1,7 @@
+string1 = 'Bhargav'
+print(len(string1))
+
+lst = ['thing1', 'thing__2']
+print(len(lst))
+# That is polymorphism, a single function knows how to handle
+# different kinds of objects as expected
